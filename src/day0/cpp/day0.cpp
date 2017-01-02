34a84fb8a15405c7e8eee353e5ad62c4d17e2eaf
Miguel Rentes <miguel.rentes@gmail.com>
2017-01-02 19:27:32 +0000

    Adds mean and median calculations.

--- src/day0/cpp/day0.cpp
+++ src/day0/cpp/day0.cpp
@@ -2,8 +2,47 @@
 // Created by Miguel Rentes on 01/01/17.
 //
 #include "../headers/day0.h"
-using namespace std;
 
-int main(int argc, char** argv) {
-    cout << "test" << endl;
+int main(void) {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    string N = "";
+    unsigned int number_of_elements = 0;
+    getline(cin, N);
+    number_of_elements = atoi(N.c_str());
+
+    unsigned int n;
+    vector<unsigned int> nums;
+    for(int i ; i < number_of_elements; ++i)
+        if(cin >> n)
+            nums.push_back(n);
+
+    // mean
+    float mean = 0.0f;
+    unsigned int sum = 0;
+    for(int i ; i < number_of_elements; ++i)
+        sum += nums[i];
+    mean = (float) sum / number_of_elements;
+
+    // median
+    float median = 0.0f;
+    sort(nums.begin(), nums.end());
+    if (number_of_elements % 2 == 0) {
+        median = (float) (nums[(number_of_elements/2)-1] + nums[(number_of_elements/2)]) / 2;
+    } else median = (float) nums[number_of_elements/2];
+
+    //for(int i ; i < number_of_elements; ++i)
+    //    cout << nums[i] << endl;
+
+    // mode
+    unsigned int mode = 0;
+    if (number_of_elements == 1 || number_of_elements == 2)
+        mode = nums[1];
+    else {
+
+    }
+
+    cout.precision(1);
+    cout << fixed << mean << endl;
+    cout << fixed << median << endl;
+    return EXIT_SUCCESS;
 }
--- src/day0/headers/day0.h
+++ src/day0/headers/day0.h
@@ -4,5 +4,14 @@
 
 #ifndef HACKER_RANK_BASIC_STATISTICS_DAY0_H
 #define HACKER_RANK_BASIC_STATISTICS_DAY0_H
+#include <cmath>
+#include <cstdio>
+#include <vector>
 #include <iostream>
+#include <algorithm>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+#include <limits>
+using namespace std;
 #endif //HACKER_RANK_BASIC_STATISTICS_DAY0_H
